c2b0122cf1da3895953e020ea0d661595c4ae991
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-11-17 21:56:04 +0100

    Feature - added autocomplete

--- src/extension.ts
+++ src/extension.ts
@@ -4,28 +4,36 @@
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// the extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "yorlang-vs-code-extension" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    //let yorlang = vscode.languages.registerHoverProvider
+    let disposable = vscode.languages.registerCompletionItemProvider("yorlang", new YorlangAutoComplete());
 
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class YorlangAutoComplete implements vscode.CompletionItemProvider {
+    
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
+        const tí = new vscode.CompletionItem("tí", vscode.CompletionItemKind.Keyword);
+        const nígbàtí = new vscode.CompletionItem("nígbàtí", vscode.CompletionItemKind.Keyword);
+        const sọpé = new vscode.CompletionItem("sọpé", vscode.CompletionItemKind.Keyword);
+        const tàbí = new vscode.CompletionItem("tàbí", vscode.CompletionItemKind.Keyword);
+        const òótó = new vscode.CompletionItem("òótó", vscode.CompletionItemKind.Keyword);
+        const iró = new vscode.CompletionItem("iró", vscode.CompletionItemKind.Keyword);
+        const iṣé = new vscode.CompletionItem("iṣé", vscode.CompletionItemKind.Keyword);
+        const fún = new vscode.CompletionItem("fún", vscode.CompletionItemKind.Keyword);
+        const padà = new vscode.CompletionItem("padà", vscode.CompletionItemKind.Keyword);
+        const kúrò = new vscode.CompletionItem("kúrò", vscode.CompletionItemKind.Keyword);
+        const ṣé = new vscode.CompletionItem("ṣé", vscode.CompletionItemKind.Keyword);
+        const yí = new vscode.CompletionItem("yí", vscode.CompletionItemKind.Keyword);
+        const ẹjó = new vscode.CompletionItem("ẹjó", vscode.CompletionItemKind.Keyword);
+        const padàsí = new vscode.CompletionItem("ṣé", vscode.CompletionItemKind.Keyword);
+
+        return [tí, nígbàtí, sọpé, tàbí, òótó, iró, iṣé, fún, padà, kúrò, ṣé, yí, ẹjó, padàsí]
+    }
 }
